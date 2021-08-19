487757c00fee58342c5944a55e9a6bf33e4894ec
Harold Brooks <vintner@live.com>
2021-08-18 23:48:20 -0400

    added point and click movement

---
+++ Assets/The Great Fleece/Game/Scripts/Player.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         _agent = GetComponent<NavMeshAgent>();
+        if (_agent == null)
+        {
+            Debug.LogError("Player NavMeshAgent is null");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if left click
+        if (Input.GetMouseButtonDown(0))
+        {
+            //cast ray from mouse position
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                Debug.Log("Mouse position at " + hit.point);
+                //create object at floor position
+                GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                sphere.transform.position = hit.point;
+                _agent.destination = hit.point;
+            }
+        }
+    }
+}

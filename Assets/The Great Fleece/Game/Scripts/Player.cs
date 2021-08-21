0b6b7aea1202a9adac8a92877d4611855a3ffc3e
Harold Brooks <vintner@live.com>
2021-08-20 21:41:40 -0400

    added player animation for movement

--- Assets/The Great Fleece/Game/Scripts/Player.cs
+++ Assets/The Great Fleece/Game/Scripts/Player.cs
@@ -6,6 +6,7 @@ using UnityEngine.AI;
 public class Player : MonoBehaviour
 {
     private NavMeshAgent _agent;
+    private Animator _animator;
 
     // Start is called before the first frame update
     void Start()
@@ -13,23 +14,36 @@ public class Player : MonoBehaviour
          _agent = GetComponent<NavMeshAgent>();
         if (_agent == null)
         {
-            Debug.LogError("Player NavMeshAgent is null");
+            Debug.LogError("Player NavMeshAgent is null.");
+        }
+        _animator = GetComponentInChildren<Animator>();
+        if (_animator == null)
+        {
+            Debug.LogError("Animator on player is null.");
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if left click
+        //if left click mouse
         if (Input.GetMouseButtonDown(0))
         {
             //cast ray from mouse position
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out RaycastHit hit))
             {
-                Debug.Log("Mouse position at " + hit.point);
                 _agent.destination = hit.point;
             }
+        }
+        //animate walking if moving to destination
+        if (_agent.hasPath == true)
+        {
+            _animator.SetBool("Walk", true);
+        }
+        else
+        {
+            _animator.SetBool("Walk", false);
         }
     }
 }
